5e99c55e4e83aeacb361439b7ac129219f83703c
Audrey <audreybrown94@gmail.com>
2018-01-20 19:59:13 -1000

    working on recursion looping

--- js/letters.js
+++ js/letters.js
@@ -16,7 +16,25 @@ var counter = {
 }
 
 function countLetters(counter, sample_text){
-  // FIX ME
+  var counter = {
+    "a" : 0,  "b" : 0,  "c" : 0,  "d" : 0,
+    "e" : 0,  "f" : 0,  "g" : 0,  "h" : 0,
+    "i" : 0,  "j" : 0,  "k" : 0,  "l" : 0,
+    "m" : 0,  "n" : 0,  "o" : 0,  "p" : 0,
+    "q" : 0,  "r" : 0,  "s" : 0,  "t" : 0,
+    "u" : 0,  "v" : 0,  "w" : 0,  "x" : 0,
+    "y" : 0,  "z" : 0
+  }
+  let n = 0;
+  if (n === 29) {
+    return "";
+  }
+sample_text.charAt(n);
+let letter = sample_text[n]
+counter.letter ++;
+
+n++
+countLetters(counter,sample_text);
 }
 
 $(document).ready(function(){
