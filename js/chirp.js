bac0e7df953aed65d1626a646d729a6574ee7707
Audrey <audreybrown94@gmail.com>
2018-01-20 19:17:32 -1000

    completed

--- js/chirp.js
+++ js/chirp.js
@@ -1,6 +1,11 @@
 function chirp(n){
-  // FIX ME
-}
+  if (n === 0){ 
+    return "";
+  }
+  n--;
+  return "chirp " + chirp(n);
+};
+
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
